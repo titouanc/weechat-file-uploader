68c7791b078c5552a509b8eb1c1a7d4c276fdd6b
iTitou <moiandme@gmail.com>
2021-07-13 12:09:59 +0200

    [enh] Add hashing algo in settings + add comments in settings
    Signed-off-by: iTitou <moiandme@gmail.com>

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from os import path
-from hashlib import sha256, sha512
+import hashlib
 
 from flask import Flask, request
 from flask_httpauth import HTTPBasicAuth
 
-from settings import FILE_UPLOAD_DIR, FILE_PUBLIC_URL, AUTHORIZED_USERS
+from settings import FILE_UPLOAD_DIR, FILE_PUBLIC_URL, FILE_HASH_ALGO, AUTHORIZED_USERS
 
 
+file_hash_algo = getattr(hashlib, FILE_HASH_ALGO)
 auth = HTTPBasicAuth()
+app = Flask(__name__)
 
 
 @auth.verify_password
 def auth_user(username, password):
     if username in AUTHORIZED_USERS:
-        hashed = sha512(password.encode()).hexdigest()
+        hashed = hashlib.sha512(password.encode()).hexdigest()
         return AUTHORIZED_USERS[username] == hashed
     return False
-
-
-app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
@@ -27,7 +26,7 @@ def upload():
     assert request.method == 'POST'
 
     f = request.files['file']
-    name = sha256(f.stream.read()).hexdigest()
+    name = file_hash_algo(f.stream.read()).hexdigest()
     ext = f.filename.split('.', 1)[-1]
     save_name = f"{name}.{ext}"
 
--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
-FILE_UPLOAD_DIR = '/some/directory'
-FILE_PUBLIC_URL = 'https://localhost'
-AUTHORIZED_USERS = {
-    # username: sha512(password).hexdigest()
-}
+# Directory on the local filesystem where the files should be uploaded
+FILE_UPLOAD_DIR = '/some/directory'  # __LOCAL__
+
+# Public URL at which the uploaded diles are served
+FILE_PUBLIC_URL = 'http://localhost'  # __LOCAL__
+
+# The hashing algorithm to determine the uploaded file name
+FILE_HASH_ALGO = 'sha256'  # __LOCAL__
+
+# List of users:password that are allowed to upload on this app
+AUTHORIZED_USERS = {  # __LOCAL__
+    # username: sha512(password).hexdigest()  # __LOCAL__
+}  # __LOCAL__
 
 try:
     from local_settings import *
